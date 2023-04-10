e04897a9eb3a1a0db6cad2bb985eedd329655957
lunainfotec <kirtimadan1997@gmail.com>
2023-04-10 20:28:40 +0530

    added : employee attendance

--- src/com/bl/employeewage/EmployeeWageDay8.java
+++ src/com/bl/employeewage/EmployeeWageDay8.java
@@ -1,15 +1,10 @@
 package com.bl.employeewage;
 
 public class EmployeeWageDay8 {
-      String message;
-    void msg(){
-        message = " Welcome to Employee Wage Computation Program";
-        System.out.println(message);
-    }
+    int emp_is_full_time = 1;
+
 
     void  empcheck(){
-
-        int emp_is_full_time = 1;
         int employeecheck = (int)(Math.random() * 10 % 2);
 
         if (employeecheck == 1){
@@ -17,15 +12,9 @@ public class EmployeeWageDay8 {
         }else {
             System.out.println("Employee not present");
         }
-
     }
     public static void main(String[] args) {
         EmployeeWageDay8 obj = new EmployeeWageDay8();
-        obj.msg();
         obj.empcheck();
-
-
-
     }
-
 }
