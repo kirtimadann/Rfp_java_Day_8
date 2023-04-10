9e93bcc97a927ed5cfd53ed8c6f20df0f6e833cc
lunainfotec <kirtimadan1997@gmail.com>
2023-04-10 23:51:03 +0530

    addred : total employee wage

--- src/com/bl/employeewage/EmployeeWageDay8.java
+++ src/com/bl/employeewage/EmployeeWageDay8.java
@@ -4,42 +4,50 @@ public class EmployeeWageDay8 {
    public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
 
-    int Empoloyee_per_rate_hrs = 20;
+    public static final int EMPLOYEE_PER_RATEHRS = 20;
 
-    void empwageUC4(){
+    public static final int NUM_OF_WORKINGDAY = 2;
 
-        int empperhrs= 0;
+    void empwageUC5(){
+
+        int empperhrs;
         int empwage = 0;
+        int totalempwage =0;
 
+for(int day = 0; day < NUM_OF_WORKINGDAY; day ++) {
 
-        int employeecheck = (int)(Math.random() * 10 % 3);
+    int employeecheck = (int) (Math.random() * 10 % 3);
 
-        switch (employeecheck){
+    switch (employeecheck) {
+        case IS_PART_TIME:
+            empperhrs = 4;
+            break;
 
-            case IS_PART_TIME:
-                empperhrs = 4;
-                break;
+        case IS_FULL_TIME:
+            empperhrs = 8;
+            break;
 
-            case IS_FULL_TIME:
-                empperhrs = 8;
-                break;
-            default:{
-                empperhrs =0;
-            }
-
+        default: {
+            empperhrs = 0;
         }
 
 
-
-
-        empwage = empperhrs * Empoloyee_per_rate_hrs;
+    }
+        empwage = empperhrs * EMPLOYEE_PER_RATEHRS;
+        System.out.println(empwage);
+        totalempwage += empwage;
         System.out.println("Employee Wage :" + empwage);
 
     }
+
+    System.out.println("Total Employee Wage" + totalempwage);
+}
+
+
     public static void main(String[] args) {
 
         EmployeeWageDay8 obj = new EmployeeWageDay8();
-        obj.empwageUC4();
+        obj.empwageUC5();
 
     }
 }
