088cdd439419e204ab0d7417a5340fa9f5c9f4ff
lunainfotec <kirtimadan1997@gmail.com>
2023-04-10 23:32:33 +0530

    addred : switch case exmple

--- src/com/bl/employeewage/EmployeeWageDay8.java
+++ src/com/bl/employeewage/EmployeeWageDay8.java
@@ -1,11 +1,12 @@
 package com.bl.employeewage;
 
 public class EmployeeWageDay8 {
-    int is_part_time = 1;
-    int is_full_time = 2;
+   public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+
     int Empoloyee_per_rate_hrs = 20;
 
-    void empwageUC3(){
+    void empwageUC4(){
 
         int empperhrs= 0;
         int empwage = 0;
@@ -13,16 +14,23 @@ public class EmployeeWageDay8 {
 
         int employeecheck = (int)(Math.random() * 10 % 3);
 
-        if (employeecheck == is_part_time){
+        switch (employeecheck){
 
-            empperhrs = 4;
+            case IS_PART_TIME:
+                empperhrs = 4;
+                break;
 
-        } else if (employeecheck == is_full_time) {
-            empperhrs = 8;
+            case IS_FULL_TIME:
+                empperhrs = 8;
+                break;
+            default:{
+                empperhrs =0;
+            }
 
-        } else {
-            empperhrs = 0;
         }
+
+
+
 
         empwage = empperhrs * Empoloyee_per_rate_hrs;
         System.out.println("Employee Wage :" + empwage);
@@ -31,7 +39,7 @@ public class EmployeeWageDay8 {
     public static void main(String[] args) {
 
         EmployeeWageDay8 obj = new EmployeeWageDay8();
-        obj.empwageUC3();
+        obj.empwageUC4();
 
     }
 }
