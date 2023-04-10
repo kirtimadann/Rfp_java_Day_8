25a1b8ebf5528516172d260a8ca35b12b55b3a59
lunainfotec <kirtimadan1997@gmail.com>
2023-04-10 19:15:12 +0530

    added : welcome message

---
+++ src/com/bl/employeewage/EmployeeWageDay8.java
@@ -0,0 +1,19 @@
+package com.bl.employeewage;
+
+public class EmployeeWageDay8 {
+
+      String message;
+
+    void msg(){
+
+        message = "Displaying Welcome to Employee Wage Computation Program";
+
+        System.out.println(message);
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
